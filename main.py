0f64648cb4b4caebfca51ae29c016b7dfa858d32
Manfredo Fraccola <manfredo.fraccola@gmail.com>
2021-12-04 15:43:40 +0100

    Riorganizzazione file e cartelle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from my_dash_app.maindash import app
 from my_dash_app.views.layout1 import make_layout
 server = app.server
-
+app.layout = make_layout()
 if __name__ == '__main__':
-    app.layout = make_layout()
     app.run_server(debug=True)
