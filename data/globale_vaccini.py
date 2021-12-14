52d308b0b0c6e9fc0800d222815c71e2d39e3e36
Andrea <andreino2000@gmail.com>
2021-12-14 22:09:51 +0100

    Nomi colonne

--- data/globale_vaccini.py
+++ data/globale_vaccini.py
@@ -6,6 +6,9 @@ vaccini_globali = vaccini_globali[vaccini_globali["location"] == "World"]
 keep = ["location", "date", "daily_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_boosters"]
 vaccini_globali = vaccini_globali[keep]
 vaccini_globali = vaccini_globali.rename(columns={"date": "data", "location": "paese",
-                                                  "daily_vaccinations": "vaccinazioni"})
+                                                  "daily_vaccinations": "nuovi_vaccinati",
+                                                  "people_vaccinated": "totale_vaccinati",
+                                                  "people_fully_vaccinated": "totale_vaccinati_completi",
+                                                  "total_boosters": "terza_dose"})
 vaccini_globali.to_csv("vaccini_globali.csv")
 print(vaccini_globali.head())
