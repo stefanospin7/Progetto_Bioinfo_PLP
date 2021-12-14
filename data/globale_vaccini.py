acec43f76cee1396062356884d0f1187dcff3db1
Andrea <andreino2000@gmail.com>
2021-12-14 21:34:02 +0100

    Spostato globale_vaccini.py in /data

--- data/globale_vaccini.py
+++ data/globale_vaccini.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-vaccini_globali = pd.read_csv("data/global_vaccinations.csv", parse_dates=["date"])
+vaccini_globali = pd.read_csv("global_vaccinations.csv", parse_dates=["date"])
 vaccini_globali = vaccini_globali[vaccini_globali["location"] == "World"]
 keep = ["location", "date", "daily_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_boosters"]
 vaccini_globali = vaccini_globali[keep]
