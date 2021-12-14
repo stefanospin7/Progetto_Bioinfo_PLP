97b2cdc7fb1df37f6b1d3208de36cea9c6a93357
manfrifrac <manfrifrac@gmail.com>
2021-12-14 21:40:11 +0000

    update data

---
+++ classeAnalisi.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as ex
+class Analisi:
+    def __init__(self, csv):
+        self.df = pd.read_csv(csv)
+
+        self.fig = go.Figure(
+            go.Bar(hoverinfo='skip',
+                    x=self.df["data"],
+                    y=self.df["deceduti"]
+                   )
+            )
+
+italia = Analisi("data/datiCovidItalia.csv")
+italia.fig.show()
