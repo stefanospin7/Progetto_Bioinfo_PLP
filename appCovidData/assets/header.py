8e223a687e228a546e4fc265f322cebc9bbf602a
Manfredo Fraccola <manfredo.fraccola@gmail.com>
2022-01-03 16:15:48 +0100

    change layout

--- appCovidData/assets/header.py
+++ appCovidData/assets/header.py
@@ -22,6 +22,25 @@ header = dbc.Container(dbc.Navbar(
                                 style={"textDecoration": "none"},
                             )
                         ),
+                        dbc.Col(
+                            html.A(
+                                # Use row and col to control vertical alignment of logo / brand
+                                dbc.Row(
+                                    [
+                                        dbc.Col([
+                                            html.I(className="fab fa-github d-inline me-1"),
+                                            html.P(children="Git-Hub app repository", className="d-inline"),
+                                            ],
+                                            width="auto",
+                                            className="ms-auto"),
+                                    ],
+                                    align="center",
+                                    className="g-0",
+                                ),
+                                href="https://github.com/stefanospin7/Progetto_Bioinfo_PLP",
+                                style={"textDecoration": "none"},
+                            )
+                        ),
                     ],
                     align="center",
                     className="g-0 w-100",
