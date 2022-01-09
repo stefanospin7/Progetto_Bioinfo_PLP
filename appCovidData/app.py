1c3c2c85a540cf7f0a3d8db33497c6c0f7e8c083
GaiaTomei <94674689+GaiaTomei@users.noreply.github.com>
2022-01-09 19:49:59 +0100

    Update app.py

--- appCovidData/app.py
+++ appCovidData/app.py
@@ -1,7 +1,7 @@
-import dash  # layout html
+import dash  
 import dash_bootstrap_components as dbc
-from dash import html, Input, Output, callback_context
 
+#creazione web app dash 
 app = dash.Dash(__name__,
                 title="COWID - a COVID-19 dashboard",
                 suppress_callback_exceptions=True,
@@ -12,19 +12,17 @@ app = dash.Dash(__name__,
                         'property': 'og:site_name',
                         'content': 'COWID - a COVID-19 dashboard',
                     },
-                    # A description of the app, used by e.g.
-                    # search engines when displaying search results.
                     {
                         'property': 'og:title',
                         'content': 'COWID - a COVID-19 dashboard',
                     },
-                    # A description of the app, used by e.g.
-                    # search engines when displaying search results.
                     {
                         'property': 'og:description',
                         'content': 'COWID - a COVID-19 dashboard',
                     },
+                  #set viewport for mobile
                     {"name": "viewport", "content": "width=device-width, initial-scale=1"},
                 ],
+                #import external stylesheets libraries
                 external_stylesheets=[dbc.themes.DARKLY, dbc.icons.BOOTSTRAP, 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'],
                 )
