803b65d18c2fab213ab6ee81934e2e0214edd3b9
Manfredo Fraccola <manfredo.fraccola@gmail.com>
2022-01-09 20:36:16 +0100

    fix deleted callback import

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from appCovidData.app import app
 from appCovidData.page_layouts.layout2 import make_layout as pag1
-from dash import html, Input, Output, dcc  # funzioni di layout html interattivo
+from dash import html, dcc
+from appCovidData import callbacks
 
 #creo un server dash 
 server = app.server
